2cd9be97eb6a99edc51c4238ce78262b4f827ab8
Dylan Smith <dylan@rh.izo.me>
2017-06-14 15:42:53 -0700

    Now linking php-fpm conf files.

--- web/test.php
+++ web/test.php
@@ -1,1 +1,20 @@
-<?php echo phpinfo(); ?>
+<?php
+
+// echo phpinfo();
+
+class A {
+  public function test() {
+    echo "Class A is echoing";
+  }
+}
+
+class B extends A {
+  public function test() {
+    parent::test();
+    echo "Class B is echoing";
+  }
+}
+
+$a = new A();
+$b = new B();
+$b->test();
