53cfa1bc86af201d26a99fce2623d8dbd2a3e1c1
Dylan Smith <dylan@rh.izo.me>
2017-06-14 17:14:34 -0700

    wip

--- web/test.php
+++ web/test.php
@@ -1,20 +1,4 @@
 <?php
 
-// echo phpinfo();
+echo phpinfo();
 
-class A {
-  public function test() {
-    echo "Class A is echoing";
-  }
-}
-
-class B extends A {
-  public function test() {
-    parent::test();
-    echo "Class B is echoing";
-  }
-}
-
-$a = new A();
-$b = new B();
-$b->test();
